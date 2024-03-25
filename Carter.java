4713afa9062674bbd761dfc13ae8fc1c30ab85be
Vladimir Herdman <147741813+Vladimir-Herdman@users.noreply.github.com>
2024-03-25 12:06:13 -0400

    Adding the files
    I just downloaded from eclipse and uploaded here

---
+++ Carter.java
@@ -0,0 +1,329 @@
+
+public class Carter
+{
+private String name;
+  
+  private boolean buff;
+  private boolean throwingAbility;
+  private boolean hotDogEating;
+  private boolean brain;
+  private boolean kindness;
+  private boolean wealth;
+  private boolean cool;
+  private boolean sad;
+  private boolean friend;
+  private boolean fat;
+  private boolean baby;
+  private boolean pumpBlessing;
+  private boolean tren;
+  
+  private boolean win;
+
+  private boolean dead;
+  private boolean hurt;
+  
+  private boolean sword;
+  private boolean superSword;
+  private boolean armor;
+  private boolean superArmour;
+  private boolean goblinTeeth;
+  private boolean healthPotion;
+  private boolean strengthPotion;
+  private boolean speedPotion;
+  private boolean acid;
+  private boolean dumb;
+  private boolean cursed;
+  private boolean insurance;
+  	
+  public Carter(String n, boolean hotDog)
+    {
+		name = n;
+      	buff = false;
+      	throwingAbility = false;
+    	hotDogEating = hotDog;
+    	brain = false;
+    	kindness = false;
+    	wealth = false;
+    	cool = false;
+    	sad = false;
+    	friend = false;
+    	fat = false;
+   		baby = false;
+   		pumpBlessing = false;
+    	tren = false;
+    	dead = false;
+    	hurt = false;
+    	baby = false;
+    	sword = false;
+    	superSword = false;
+    	armor = false;
+    	superArmour = false;
+    	goblinTeeth = false;
+    	healthPotion = false;
+    	strengthPotion = false;
+    	acid = false;
+    	dumb = false;
+    	cursed = false;
+    	insurance = false;
+    }
+  
+  public String getName()
+    {
+     	return name;
+    }
+  
+  
+  public boolean getBuff()
+  	{
+		return buff;
+  	}
+  public void setBuff(boolean x)
+  	{
+  		buff = x;  
+  	}
+    
+  public boolean getThrow()
+   	{
+		return throwingAbility;
+   	}
+  public void setThrow(boolean x)
+  	{
+    	throwingAbility = x;
+  	}
+   
+  public boolean getBrain()
+   	{
+		return brain;
+   	}
+  public void setBrain(boolean x)
+  	{
+    	brain = x;
+  	}
+  
+  public boolean getKindness()
+   	{
+		return kindness;
+   	}
+  public void setKindness(boolean x)
+  	{
+    	kindness = x;
+  	}
+  
+  public boolean getWealth()
+   	{
+		return wealth;
+   	}
+  public void setWealth(boolean x)
+  	{
+    	wealth = x;
+  	}
+    
+  public boolean getCool()
+   	{
+		return cool;
+   	}
+  public void setCool(boolean x)
+  	{
+    	cool = x;
+  	}
+  
+  public void setDead(boolean x)							
+  	{
+    	dead = x;
+  	}
+  public boolean getDead()								
+  	{
+    	return dead;
+  	}
+  
+  public void setHurt(boolean x)
+  	{
+   		hurt = x;
+  	}
+  public boolean getHurt()
+  	{
+    	return hurt;
+  	}
+  
+  public void setSword(boolean x)
+  	{
+   	 sword = x;
+  	}
+  public boolean getSword()
+  	{
+    	return sword;
+  	}
+  
+  public void setSuperSword(boolean x)
+  	{
+   		superSword = x;
+  	}
+  public boolean getSuperSword()
+  	{
+    	return superSword;
+  	}
+  
+  public void setArmor(boolean x)
+  	{
+   		armor = x;
+  	}
+  public boolean getArmor()
+  	{
+    	return armor;
+  	}
+  
+  public void setSuperArmour(boolean x)
+  	{
+   		superArmour = x;
+  	}
+  public boolean getSuperArmour()
+  	{
+    	return superArmour;
+  	}
+  
+  public void setGoblinTeeth(boolean x)
+  	{
+   		goblinTeeth = x;
+  	}
+  public boolean getGoblinTeeth()
+  	{
+    	return goblinTeeth;
+  	}
+  
+  public void setHealthPotion(boolean x)
+  	{
+   		healthPotion = x;
+  	}
+  public boolean getHealthPotion()
+  	{
+    	return healthPotion;
+  	}
+  
+  public void setStrengthPotion(boolean x)
+  	{
+   		strengthPotion = x;
+  	}
+  public boolean getStrengthPotion()
+  	{
+    	return strengthPotion;
+  	}
+  
+  public void setAcid(boolean x)
+  	{
+   		acid = x;
+  	}
+  public boolean getAcid()
+  	{
+		return acid;
+  	}
+  
+  public void setSad(boolean x)
+  	{
+   		sad = x;
+  	}
+  public boolean getSad()
+  	{
+		return sad;
+  	}
+    
+  public void setFriend(boolean x)
+  	{
+   		friend = x;
+  	}
+  public boolean getFriend()
+  	{
+		return friend;
+  	}
+  
+  public void setDumb(boolean x)
+  	{
+   		dumb = x;
+  	}
+  public boolean getDumb()
+  	{
+		return dumb;
+  	}
+  
+  public void setHotDog(boolean x)
+  	{
+   		hotDogEating = x;
+  	}
+  public boolean getHotDog()
+  	{
+		return hotDogEating;
+  	}
+  
+  public void setFat(boolean x)
+  	{
+   		fat = x;
+  	}
+  public boolean getFat()
+  	{
+		return fat;
+  	}
+  
+  public void setCursed(boolean x)
+  	{
+   		cursed = x;
+  	}
+  public boolean getCursed()
+  	{
+		return cursed;
+  	}
+  
+  public void setSpeedPotion(boolean x)
+  	{
+   		speedPotion = x;
+  	}
+  public boolean getSpeedPotion()
+  	{
+		return speedPotion;
+  	}
+  
+  public void setInsurance(boolean x)
+  	{
+   		insurance = x;
+  	}
+  public boolean getInsurance()
+  	{
+		return insurance;
+  	}
+  
+   public void setBaby(boolean x)
+  	{
+   		baby = x;
+  	}
+  public boolean getBaby()
+  	{
+		return baby;
+  	}
+  
+  public void setPumpBlessing(boolean x)
+  	{
+		pumpBlessing = x;
+  	}
+  public boolean getPumpBlessing()
+  	{
+    	return pumpBlessing;
+  	}
+  
+  public void setTren(boolean x)
+  	{
+		tren = x;
+  	}
+  public boolean getTren()
+  	{
+    	return tren;
+  	}
+  
+  public void setWin(boolean x)
+  	{
+		win = x;
+  	}
+  public boolean getWin()
+  	{
+    	return win;
+  	}
+  
+  
+}
